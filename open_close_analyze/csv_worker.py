8dabc1547e5dcbbd2ad93edabb6cfed05ac0a3ef
Александр <kas1yoda@gmail.com>
2020-11-01 19:34:39 +0300

    open_close_analyze V1.0

---
+++ open_close_analyze/csv_worker.py
@@ -0,0 +1,51 @@
+import csv
+
+
+class CsvReader:
+    def __init__(self, path):
+        self.path = path
+
+    def csv_to_dict(self):
+        row_array = list()
+        with open(self.path) as f:
+            records = csv.DictReader(f)
+            for row in records:
+                row_array.append(row)
+        return row_array
+
+
+class CsvDictEditor:
+    def __init__(self, csv_array):
+        self.csv_array = csv_array
+
+    def __del_extra_field(self):
+        csv_array = self.csv_array
+        for i in range(len(csv_array)):
+            del csv_array[i]["<PER>"]
+            del csv_array[i]["<TIME>"]
+            del csv_array[i]["<OPEN>"]
+            del csv_array[i]["<HIGH>"]
+            del csv_array[i]["<LOW>"]
+            del csv_array[i]["<VOL>"]
+        return csv_array
+
+    def __add_extra_field(self):
+        csv_array = self.__del_extra_field()
+        for i in range(len(csv_array)):
+            csv_array[i]["<SHORT>"] = 0
+            csv_array[i]["<LONG>"] = 0
+            csv_array[i]["<SHORT_EXIT>"] = 0
+            csv_array[i]["<LONG_EXIT>"] = 0
+            csv_array[i]["<POSITION>"] = 0
+            csv_array[i]["<TODAY_INCOME>"] = "0%"
+        return csv_array
+
+    def transformed_csv(self):
+        return self.__add_extra_field()
+
+
+if __name__ == "__main__":
+    csvArray = CsvReader('TATN_151029_201029.csv').csv_to_dict()
+    csvArray = CsvDictEditor(csvArray).transformed_csv()
+    for row in csvArray:
+        print(row)
---
+++ open_close_analyze/traider_algorithm.py
@@ -0,0 +1,136 @@
+import csv_worker
+
+
+class Algorithm:
+    def __init__(self, csv_array):
+        self.csv_array = csv_array
+        
+    def short_position(self, count_period=2):
+        csv_array = self.csv_array
+        min = count_period
+        min_pos = 0
+
+        for i in range(len(csv_array) - 1):
+            if csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"] and min_pos == 0:
+                min -= 1
+            if csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"] and min_pos == 0:
+                min = count_period
+
+            if min == 0 and min_pos == 0:
+                csv_array[i + 1]["<SHORT>"] = 1
+                csv_array[i + 1]["<POSITION>"] = -1
+                min_pos = 1
+
+            if min_pos != 0 and csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"]:
+                csv_array[i + 1]["<POSITION>"] = -1
+                min_pos += 1
+            if min_pos != 0 and csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"]:
+                csv_array[i + 1]["<POSITION>"] = -1
+                min_pos = 1
+            if min_pos == count_period + 1:
+                csv_array[i + 1]["<POSITION>"] = 0
+                csv_array[i + 1]["<SHORT_EXIT>"] = 1
+                min = count_period
+                min_pos = 0
+
+        return csv_array
+
+    def long_position(self, count_period=2):
+        csv_array = self.csv_array
+        max = count_period
+        max_pos = 0
+
+        for i in range(len(csv_array) - 1):
+            if csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"] and max_pos == 0:
+                max -= 1
+            if csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"] and max_pos == 0:
+                max = count_period
+
+            if max == 0 and max_pos == 0:
+                csv_array[i + 1]["<LONG>"] = 1
+                csv_array[i + 1]["<POSITION>"] = 1
+                max_pos = 1
+
+            if max_pos != 0 and csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"]:
+                csv_array[i + 1]["<POSITION>"] = 1
+                max_pos += 1
+
+            if max_pos != 0 and csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"]:
+                csv_array[i + 1]["<POSITION>"] = 1
+                max_pos = 1
+
+            if max_pos == count_period + 2:
+                csv_array[i + 1]["<POSITION>"] = 0
+                csv_array[i]["<LONG_EXIT>"] = 1
+                max = count_period
+                max_pos = 0
+
+        return csv_array
+
+    # def long_position(self, count_period=2):
+    #     csv_array = self.csv_array
+    #     maximum = count_period
+    #     max_pos = 0
+    #     minimum = count_period
+    #     min_pos = 0
+    #
+    #     for i in range(len(csv_array) - 1):
+    #         if csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"] and max_pos == 0 and min_pos == 0:
+    #             maximum -= 1
+    #         if csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"] and max_pos == 0 and min_pos == 0:
+    #             maximum = count_period
+    #
+    #         if csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"] and max_pos == 0 and min_pos == 0:
+    #             minimum -= 1
+    #         if csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"] and max_pos == 0 and min_pos == 0:
+    #             minimum = count_period
+    #
+    #         if maximum == 0 and max_pos == 0:
+    #             csv_array[i + 1]["<LONG>"] = 1
+    #             csv_array[i + 1]["<POSITION>"] = 1
+    #             max_pos = 1
+    #
+    #         if minimum == 0 and min_pos == 0:
+    #             csv_array[i + 1]["<SHORT>"] = 1
+    #             csv_array[i + 1]["<POSITION>"] = -1
+    #             min_pos = 1
+    #
+    #         if max_pos != 0 and csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"]:
+    #             csv_array[i + 1]["<POSITION>"] = 1
+    #             max_pos += 1
+    #         if max_pos != 0 and csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"]:
+    #             csv_array[i + 1]["<POSITION>"] = 1
+    #             max_pos = 1
+    #
+    #         if min_pos != 0 and csv_array[i]["<CLOSE>"] < csv_array[i + 1]["<CLOSE>"]:
+    #             csv_array[i + 1]["<POSITION>"] = -1
+    #             min_pos += 1
+    #         if min_pos != 0 and csv_array[i]["<CLOSE>"] > csv_array[i + 1]["<CLOSE>"]:
+    #             csv_array[i + 1]["<POSITION>"] = -1
+    #             min_pos = 1
+    #
+    #         if min_pos == count_period + 1:
+    #             csv_array[i + 1]["<POSITION>"] = 0
+    #             min_pos = 0
+    #
+    #         if max_pos == count_period + 1:
+    #             csv_array[i + 1]["<POSITION>"] = 0
+    #             csv_array[i + 1]["<LONG_EXIT>"] = 1
+    #             max_pos = 0
+    #
+    #     return csv_array
+
+
+if __name__ == "__main__":
+    csvArray = csv_worker.CsvReader('TATN_151029_201029.csv').csv_to_dict()
+    csvArray = csv_worker.CsvDictEditor(csvArray).transformed_csv()
+    csvAlgorithm = Algorithm(csvArray)
+    csv = csvAlgorithm.short_position()
+    csvAlgorithm = Algorithm(csv)
+    csv = csvAlgorithm.long_position()
+    # csvAlgorithm = Algorithm(csv)
+    # csv = csvAlgorithm.short_position()
+
+
+    for row in csv:
+        print(row)
